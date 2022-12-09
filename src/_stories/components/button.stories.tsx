29d12911970e53d59ec36bca3ae2b7c3c06d9344
sudorock <kslvsunil@gmail.com>
2022-12-10 04:37:03 +0530

    Bootstrap project and add basic button component

---
+++ src/_stories/components/button.stories.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Button } from "../../components/button";
+
+export default {
+  title: "Button",
+};
+
+export const Primary = () => {
+  return (
+    <div className="flex-column flex gap-10">
+      <div className="flex gap-10">
+        <Button value={"Button"} variant={{ type: "primary", size: "xs" }} />
+        <Button value={"Button"} variant={{ type: "primary", size: "sm" }} />
+      </div>
+    </div>
+  );
+};
+
+export const Tertiary = () => {
+  return (
+    <div className="flex-column flex gap-10">
+      <div className="flex gap-10">
+        <Button value={"Button"} variant={{ type: "tertiary", size: "xs" }} />
+        <Button value={"Button"} variant={{ type: "tertiary", size: "sm" }} />
+      </div>
+    </div>
+  );
+};
---
+++ src/components/button.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { cva, VariantProps } from "cva";
+
+const variant = cva(
+  "flex font-medium justify-center w-max cursor-pointer items-center rounded-md",
+  {
+    variants: {
+      type: {
+        primary: "bg-blue-600 text-white",
+        tertiary: "bg-white text-gray-700 border border-gray-300",
+      },
+      size: {
+        xs: "text-xs gap-2 px-3 py-2 shadow-sm",
+        sm: "text-sm gap-3 px-4 py-2.5 shadow",
+      },
+    },
+    defaultVariants: {
+      type: "primary",
+      size: "sm",
+    },
+  }
+);
+
+export type Variant = VariantProps<typeof variant>;
+
+interface Props extends React.ComponentPropsWithoutRef<"button"> {
+  value: string;
+  variant: Variant;
+}
+
+variant();
+
+export const Button = ({ value, variant: { size, type }, ...props }: Props) => {
+  return (
+    <button {...props} className={variant({ size, type })}>
+      {value}
+    </button>
+  );
+};
