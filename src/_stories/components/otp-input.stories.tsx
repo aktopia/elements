0ddd691705098a47dff79e64b8a7bbfaf75dd67d
sudorock <kslvsunil@gmail.com>
2022-12-11 04:38:20 +0530

    Stories for OTP input

--- src/_stories/components/otp-input.stories.tsx
+++ src/_stories/components/otp-input.stories.tsx
@@ -4,9 +4,8 @@ import { OTPInput } from "components/otp-input";
 export const Examples = () => {
   return (
     <div className="flex flex-col gap-10">
-      <div className="flex gap-10">
-        <OTPInput num={6} onInputComplete={console.log} />
-      </div>
+      <OTPInput num={6} onInputComplete={console.log} />
+      <OTPInput num={4} onInputComplete={console.log} />
     </div>
   );
 };
