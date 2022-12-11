01987683e1b93f297beea4b8c8f076a12cc67ebe
sudorock <kslvsunil@gmail.com>
2022-12-11 23:02:41 +0530

    Did I check in node-modules?

--- storybook/tailwind.config.cjs
+++ storybook/tailwind.config.cjs
@@ -1,5 +1,6 @@
 module.exports = {
-  content: ["./src/**/*.{html,jsx,tsx,ts,js}",
+  content: [
+    "./src/**/*.{html,jsx,tsx,ts,js}",
     "../main/src/**/*.{html,jsx,tsx,ts,js}"],
   theme: {
     extend: {}
