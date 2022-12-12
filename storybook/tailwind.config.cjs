1b98fbf8433d64b76e401cb313a5f41b1d41239f
sudorock <kslvsunil@gmail.com>
2022-12-13 03:01:40 +0530

    Update tailwind.config.cjs

--- storybook/tailwind.config.cjs
+++ storybook/tailwind.config.cjs
@@ -1,10 +1,19 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./src/**/*.{html,jsx,tsx,ts,js,mdx}",
     "../lib/src/**/*.{html,jsx,tsx,ts,js,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      screens: {
+        ahover: { raw: "(hover: hover)" }, //https://github.com/tailwindlabs/tailwindcss/discussions/1739#discussioncomment-56282
+      },
+    },
   },
   plugins: [
     require("@tailwindcss/forms"),
