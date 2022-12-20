103624fc19fc4c3f75634121396e6b1d433ee89d
sudorock <kslvsunil@gmail.com>
2022-12-20 18:37:39 +0530

    SignIn component updates

--- lib/src/compositions/sign-in/sign-in.tsx
+++ lib/src/compositions/sign-in/sign-in.tsx
@@ -5,21 +5,39 @@ import React, { useCallback, useState } from "react";
 
 const loginOpts = [
   { id: "phone", label: "Phone" },
-  { id: "email", label: "Email" }
+  { id: "email", label: "Email" },
 ];
 
 interface ISignIn {
-  onSendPhoneOTP: Function,
-  onEmailSendOTP: Function,
-  onClose: Function,
-  t: { title: Function, sendOTP: Function }
+  onSendPhoneOTP: Function;
+  onSendEmailOTP: Function;
+  onClose: Function;
+  t: { title: Function; sendOTP: Function };
 }
 
-export const SignIn = ({ onSendPhoneOTP, onEmailSendOTP, onClose, t }: ISignIn) => {
+export const SignIn = ({
+  onSendPhoneOTP,
+  onSendEmailOTP,
+  onClose,
+  t,
+}: ISignIn) => {
   const [activeSwitch, setActiveSwitch] = useState("phone");
+  const [phone, setPhone] = useState<string>();
+  const [email, setEmail] = useState<string>();
   const onSendOTP = useCallback(() => {
-    activeSwitch == "phone" ? onSendPhoneOTP() : onEmailSendOTP;
-  }, [activeSwitch]);
+    activeSwitch == "phone" ? onSendPhoneOTP(phone) : onSendEmailOTP(email);
+  }, [activeSwitch, phone, email]);
+
+  const onInputChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      activeSwitch == "phone"
+        ? setPhone(e.target.value)
+        : setEmail(e.target.value);
+    },
+    [activeSwitch]
+  );
+
+  const inputValue = activeSwitch == "phone" ? phone : email;
 
   return (
     <Modal title={t.title()} onClose={onClose}>
@@ -31,7 +49,9 @@ export const SignIn = ({ onSendPhoneOTP, onEmailSendOTP, onClose, t }: ISignIn) 
           onSwitchClick={setActiveSwitch}
         />
         <input
+          value={inputValue}
           type={"text"}
+          onChange={onInputChange}
           className={
             "h-max w-[360px] rounded-md border border-gray-300 bg-gray-50 py-2 px-3 text-xl font-medium text-gray-600 shadow-inner"
           }
