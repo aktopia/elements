ed9cc11e08da6d10fc4a9703ff5eb77bbec61cce
sudorock <kslvsunil@gmail.com>
2022-12-23 03:18:10 +0530

    Export alert

--- lib/src/components.ts
+++ lib/src/components.ts
@@ -4,3 +4,4 @@ export { Modal } from '@elements/components/modal';
 export { NamedSwitch } from '@elements/components/named-switch';
 export { OtpInput } from '@elements/components/otp-input';
 export { SingleCharacterInput } from '@elements/components/single-character-input';
+export { Alert } from '@elements/components/alert';
