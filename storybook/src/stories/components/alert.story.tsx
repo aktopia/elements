59156887daf1967347d22c5a232cbd4785a309bb
sudorock <kslvsunil@gmail.com>
2022-12-24 21:59:50 +0530

    Alert component types

--- storybook/src/stories/components/alert.story.tsx
+++ storybook/src/stories/components/alert.story.tsx
@@ -6,19 +6,32 @@ export default {
 };
 
 export const Success = {
-  render: () => <Alert show={true} messageText={'Success'} variant={{ type: 'success' }} />,
+  render: () => (
+    <Alert show={true} messageText={'Success'} variant={{ type: 'success' }} onDismiss={() => {}} />
+  ),
 };
 
 export const Info = {
-  render: () => <Alert show={true} messageText={'Information'} variant={{ type: 'info' }} />,
+  render: () => (
+    <Alert
+      show={true}
+      messageText={'Information'}
+      variant={{ type: 'info' }}
+      onDismiss={() => {}}
+    />
+  ),
 };
 
 export const Warning = {
-  render: () => <Alert show={true} messageText={'Warning'} variant={{ type: 'warning' }} />,
+  render: () => (
+    <Alert show={true} messageText={'Warning'} variant={{ type: 'warning' }} onDismiss={() => {}} />
+  ),
 };
 
 export const Error = {
-  render: () => <Alert show={true} messageText={'Error'} variant={{ type: 'error' }} />,
+  render: () => (
+    <Alert show={true} messageText={'Error'} variant={{ type: 'error' }} onDismiss={() => {}} />
+  ),
 };
 
 // https://github.com/storybookjs/storybook/issues/12153
