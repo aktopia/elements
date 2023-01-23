8d1c52b5301c7e35611d64d3c982a692e4bf4d9b
sudorock <kslvsunil@gmail.com>
2023-01-24 02:12:05 +0530

    Simplify store

--- lib/src/store.ts
+++ lib/src/store.ts
@@ -1,33 +1,10 @@
 import { createContext, useContext, useSyncExternalStore } from 'react';
 
 export type Subscribe = (onStoreChange: () => void) => () => void;
+
 export type Read = (key: string, args: Array<any>) => any;
+
 export type Dispatch = (key: string, args: Array<any>) => void;
-
-interface Store {
-  subscribe: Subscribe;
-  read: Read;
-  dispatch: Dispatch;
-}
-
-class Store {
-  constructor(subscribe: Subscribe, read: Read, dispatch: Dispatch) {
-    this.subscribe = subscribe;
-    this.read = read;
-    this.dispatch = dispatch;
-  }
-}
-
-let store: Store;
-
-export function init(subscribe: Subscribe, read: Read, dispatch: Dispatch) {
-  store = new Store(subscribe, read, dispatch);
-  Object.freeze(store);
-}
-
-export function dispatch(key: string, ...args: Array<any>) {
-  store.dispatch(key, args);
-}
 
 export const StoreContext = createContext<any>({});
 
