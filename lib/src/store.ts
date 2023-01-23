8dd77978890f06fd523ab7267b793b5317c5cb6e
sudorock <kslvsunil@gmail.com>
2023-01-24 02:22:09 +0530

    renamed key to id

--- lib/src/store.ts
+++ lib/src/store.ts
@@ -2,18 +2,18 @@ import { createContext, useContext, useSyncExternalStore } from 'react';
 
 export type Subscribe = (onStoreChange: () => void) => () => void;
 
-export type Read = (key: string, args: Array<any>) => any;
+export type Read = (id: string, args: Array<any>) => any;
 
-export type Dispatch = (key: string, args: Array<any>) => void;
+export type Dispatch = (id: string, args: Array<any>) => void;
 
 export const StoreContext = createContext<any>({});
 
-export function useValue(key: string, ...args: Array<any>) {
+export function useValue(id: string, ...args: Array<any>) {
   const { read, subscribe } = useContext(StoreContext);
-  return useSyncExternalStore(subscribe, () => read(key, args));
+  return useSyncExternalStore(subscribe, () => read(id, args));
 }
 
-export function useDispatch(key: string) {
+export function useDispatch(id: string) {
   const { dispatch } = useContext(StoreContext);
-  return (...args: Array<any>) => dispatch(key, args);
+  return (...args: Array<any>) => dispatch(id, args);
 }
