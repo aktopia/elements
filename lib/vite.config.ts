79a2859c946e4053d4a5c06352233bc4dcb9ab1a
sudorock <kslvsunil@gmail.com>
2023-01-26 20:43:13 +0530

    export store

--- lib/vite.config.ts
+++ lib/vite.config.ts
@@ -1,26 +1,27 @@
-import { defineConfig } from "vite";
-import path, { resolve } from "path";
-import react from "@vitejs/plugin-react";
-import dts from "vite-plugin-dts";
+import react from '@vitejs/plugin-react';
+import path, { resolve } from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   resolve: {
     alias: {
-      "@elements": path.resolve(__dirname, "src/")
-    }
+      '@elements': path.resolve(__dirname, 'src/'),
+    },
   },
   build: {
     sourcemap: true,
     lib: {
       entry: {
-        "components": resolve(__dirname, "src/components.ts"),
-        "compositions": resolve(__dirname, "src/compositions.ts")
+        components: resolve(__dirname, 'src/components.ts'),
+        compositions: resolve(__dirname, 'src/compositions.ts'),
+        store: resolve(__dirname, 'src/store.tsx'),
       },
-      formats: ["es"]
+      formats: ['es'],
     },
     rollupOptions: {
-      external: ["react", "react-dom"]
-    }
+      external: ['react', 'react-dom'],
+    },
   },
-  plugins: [react(), dts({ insertTypesEntry: true })]
+  plugins: [react(), dts({ insertTypesEntry: true })],
 });
