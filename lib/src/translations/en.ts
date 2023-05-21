2663f5f8dc3281fd3e32cc74d4427a46beafc72c
sudorock <kslvsunil@gmail.com>
2023-05-21 16:43:12 +0530

    Add translations for Sign in

--- lib/src/translations/en.ts
+++ lib/src/translations/en.ts
@@ -4,6 +4,9 @@ export default {
   'common/phone': 'Phone',
   'common/issue': 'Issue',
   'common/action': 'Action',
+  'common/create': 'Create',
+  'common/sign-in': 'Sign in',
+  'common/sign-out': 'Sign out',
   'relation/resolves': 'Resolves',
   'relation/partially-resolves': 'Partially Resolves',
   'relation/relates': 'Relates To',
@@ -17,4 +20,5 @@ export default {
   'auth/enter-otp': 'Enter OTP',
   'auth/invalid-otp': 'Incorrect OTP, please try again.',
   'auth.verify-otp/success': 'OTP successfully verified.',
+  'auth/sign-in-with-google': 'Sign in with Google',
 };
