ab1a10414bbf99640de7023c947d92e9b81c2001
sudorock <kslvsunil@gmail.com>
2023-05-21 16:42:54 +0530

    Add google sign in

---
+++ lib/src/compositions/auth/social-sign-in.tsx
@@ -0,0 +1,24 @@
+import { Button } from '@elements/components/button';
+import { Modal } from '@elements/components/modal';
+import { useDispatch, useValue } from '@elements/store';
+import { useTranslation } from '@elements/translation';
+
+export const SocialSignIn = () => {
+  const t = useTranslation();
+  const visible = useValue<boolean>('auth.sign-in/visible');
+  const onGoogleClick = useDispatch('auth.sign-in.google/initiate');
+  const onModalClose = useDispatch('auth.sign-in.modal/close');
+
+  return (
+    <Modal title={t('common/sign-in')} visible={visible} onClose={onModalClose}>
+      <div className={'m-6 flex w-40 flex-col items-center'}>
+        <Button
+          kind={'primary'}
+          size={'sm'}
+          value={t('auth/sign-in-with-google')}
+          onClick={onGoogleClick}
+        />
+      </div>
+    </Modal>
+  );
+};
