d6860b74c51ecf115c60ed3d86d042adfec349d4
sudorock <kslvsunil@gmail.com>
2023-06-03 07:14:26 +0530

    setContent on editor when initial value has changed

--- lib/src/components/rich-text-area.tsx
+++ lib/src/components/rich-text-area.tsx
@@ -1,5 +1,6 @@
 import { EditorContent, useEditor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
+import { memo, useEffect } from 'react';
 
 interface RichTextAreaProps {
   className?: string;
@@ -9,48 +10,48 @@ interface RichTextAreaProps {
   output?: 'html' | 'text';
 }
 
-export const RichTextArea = ({
-  className,
-  onChange,
-  initialValue,
-  output = 'html',
-  editable = false,
-}: RichTextAreaProps) => {
-  const editor = useEditor({
-    editable,
-    extensions: [
-      StarterKit.configure({
-        bulletList: {
-          keepMarks: true,
-          keepAttributes: false,
+export const RichTextArea = memo(
+  ({ className, onChange, initialValue, output = 'html', editable }: RichTextAreaProps) => {
+    const editor = useEditor({
+      editable: editable || false,
+      extensions: [
+        StarterKit.configure({
+          bulletList: {
+            keepMarks: true,
+            keepAttributes: false,
+          },
+          orderedList: {
+            keepMarks: true,
+            keepAttributes: false,
+          },
+        }),
+      ],
+      editorProps: {
+        attributes: {
+          class: `focus:outline-none ${className || ''}`.trim(),
         },
-        orderedList: {
-          keepMarks: true,
-          keepAttributes: false,
-        },
-      }),
-    ],
-    editorProps: {
-      attributes: {
-        class: `focus:outline-none ${className || ''}`.trim(),
       },
-    },
-    onUpdate: ({ editor }) => {
-      let value;
-      switch (output) {
-        case 'html':
-          value = editor.getHTML();
-          break;
-        case 'text':
-          value = editor.getText();
-          break;
-      }
-      onChange(value);
-    },
-    content: initialValue,
-  });
+      onUpdate: ({ editor }) => {
+        let value;
+        switch (output) {
+          case 'html':
+            value = editor.getHTML();
+            break;
+          case 'text':
+            value = editor.getText();
+            break;
+        }
+        onChange(value);
+      },
+      content: initialValue,
+    });
 
-  return <EditorContent editor={editor} />;
-};
+    useEffect(() => {
+      editor?.commands.setContent(initialValue, true);
+    }, [editor, initialValue]);
+
+    return <EditorContent editor={editor} />;
+  }
+);
 
 export default RichTextArea;
