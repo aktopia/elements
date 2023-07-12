89f8593e15586f44eb8a612fdadd0c94553a90d2
sudorock <kslvsunil@gmail.com>
2023-07-12 12:26:58 +0530

    introducte app loading and main component

--- lib/src/app.tsx
+++ lib/src/app.tsx
@@ -1,10 +1,25 @@
+import { Spinner } from '@elements/components/spinner';
+import { suspensify } from '@elements/components/suspensify';
 import { Alert } from '@elements/compositions/alert';
 import '@elements/index.css';
 import { Router } from '@elements/router';
-import { Store, StoreProps } from '@elements/store';
+import { Store, StoreProps, useValue } from '@elements/store';
 import { locales, Translation, TranslationProps } from '@elements/translation';
 
 interface AppProps extends StoreProps, TranslationProps {}
+
+const Main = suspensify(() => {
+  const loading = useValue<boolean>('app/loading');
+
+  return loading ? (
+    <Spinner visible={loading} />
+  ) : (
+    <>
+      <Router suspense={{ lines: 20 }} />
+      <Alert suspense={{ lines: 5 }} />
+    </>
+  );
+});
 
 export const App = ({
   subscribe,
@@ -24,8 +39,7 @@ export const App = ({
       read={read}
       subscribe={subscribe}>
       <Translation defaultLocale={defaultLocale} locales={locales} suspense={{ lines: 10 }}>
-        <Router suspense={{ lines: 20 }} />
-        <Alert suspense={{ lines: 5 }} />
+        <Main suspense={{ lines: 10 }} />
       </Translation>
     </Store>
   );
