e510cca036f03e0fc17757e5afaaecf79e182f30
sudorock <kslvsunil@gmail.com>
2023-07-23 13:14:01 +0530

    Timestamp tooltip

--- lib/src/components/_raw.tsx
+++ lib/src/components/_raw.tsx
@@ -6,4 +6,7 @@ export {
   Input as RawInput,
   Button as RawButton,
   Popover as RawPopover,
+  OverlayArrow as RawOverlayArrow,
+  Tooltip as RawTooltip,
+  TooltipTrigger as RawTooltipTrigger,
 } from 'react-aria-components';
--- lib/src/components/timestamp.tsx
+++ lib/src/components/timestamp.tsx
@@ -1,16 +1,61 @@
 import { isBefore, timeAgoStr, tsNow, tsToStr } from '@elements/_utils';
-import { useMemo } from 'react';
+import {
+  RawButton,
+  RawOverlayArrow,
+  RawTooltip,
+  RawTooltipTrigger,
+} from '@elements/components/_raw';
+import { ReactNode, useMemo } from 'react';
 
 const dateTimeFormat = 'MMM D, YYYY [at] h:mm A';
 
+const CaretDown = ({ className }: { className: string }) => {
+  return (
+    <svg
+      className={className}
+      fill={'currentColor'}
+      fillOpacity={'0.95'}
+      viewBox={'0 0 52 52'}
+      xmlSpace={'preserve'}>
+      <path
+        d={
+          'M8.3 14h35.4c1 0 1.7 1.3.9 2.2L27.3 37.4c-.6.8-1.9.8-2.5 0L7.3 16.2c-.7-.9-.1-2.2 1-2.2z'
+        }
+      />
+    </svg>
+  );
+};
+
+const TextTooltip = ({ children }: { children: ReactNode }) => {
+  return (
+    <RawTooltip
+      className={'mb-3 rounded-md bg-gray-700 py-1 px-1.5 text-xs text-white opacity-80 shadow-md'}>
+      {children}
+      <RawOverlayArrow>
+        <CaretDown className={'-mt-1.5 h-4 w-4 text-gray-700'} />
+      </RawOverlayArrow>
+    </RawTooltip>
+  );
+};
+
 export const Timestamp = ({ timestamp, className }: { timestamp: number; className: string }) => {
-  const formattedTime = useMemo(() => {
+  const [time, tooltipTime] = useMemo(() => {
     const now = tsNow();
 
+    const absoluteTime = tsToStr(timestamp, dateTimeFormat);
+    const relativeTime = timeAgoStr(timestamp);
+
     return isBefore(timestamp, now, 'month')
-      ? tsToStr(timestamp, dateTimeFormat)
-      : timeAgoStr(timestamp);
+      ? [absoluteTime, relativeTime]
+      : [relativeTime, absoluteTime];
   }, [timestamp]);
 
-  return <p className={className}>{formattedTime}</p>;
+  return (
+    <RawTooltipTrigger delay={500}>
+      <RawButton className={className}>{time}</RawButton>
+      <TextTooltip>
+        <p>{tooltipTime}</p>
+      </TextTooltip>
+    </RawTooltipTrigger>
+  );
 };
