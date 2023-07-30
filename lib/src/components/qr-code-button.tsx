8c5e3b8642ba726966a0455ed7d3bac157d1dcf3
sudorock <kslvsunil@gmail.com>
2023-07-30 21:52:08 +0530

    qr code button

---
+++ lib/src/components/qr-code-button.tsx
@@ -0,0 +1,17 @@
+import { QrCodeOutline } from '@elements/_icons';
+import { Button, ButtonProps } from '@elements/components/button';
+import { memo } from 'react';
+
+type QRCodeButtonProps = Omit<ButtonProps, 'value' | 'Icon'>;
+
+export const QRCodeButton = memo(({ clicked, ...props }: QRCodeButtonProps) => {
+  return (
+    <Button
+      {...props}
+      Icon={QrCodeOutline}
+      clicked={clicked}
+      iconClassName={'stroke-2'}
+      value={'QR Code'}
+    />
+  );
+});
