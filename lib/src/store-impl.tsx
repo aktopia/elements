454272da45398fb42d85194b3a6da4f5f6323211
indy <kslvsunil@gmail.com>
2023-08-12 21:02:26 +0530

    basic new store

--- lib/src/store-impl.tsx
+++ lib/src/store-impl.tsx
@@ -1,22 +1,65 @@
-import { useQuery } from 'react-query';
+import { useQuery as useReactQuery } from 'react-query';
+import { create } from 'zustand';
+import { useCallback } from 'react';
 
 const subscriptions: any = {};
+const events: any = {};
 
-export function regSub(id: any, fn: any) {
-  subscriptions[id] = fn;
+const useStore = create(() => ({
+  bears: 0,
+}));
+
+export function sub(id: any, fn: any) {
+  subscriptions[id] = { fn, async: false };
 }
 
-regSub('action/id', (params: any) => {
-  return params['id'];
+export function asyncSub(id: any, fn: any) {
+  subscriptions[id] = { fn, async: true };
+}
+
+asyncSub('action/id', (params: any) => {
+  return new Promise((res) => res(params['id']));
+});
+
+sub('bears', (state: any, { n }: any) => {
+  return state.bears + n;
 });
 
 export function queryFunction({ queryKey }: any) {
   const [id, params] = queryKey;
-  const sub = subscriptions[id];
-  return Promise.resolve(sub(params));
+  const sub = subscriptions[id].fn;
+  return sub(params);
 }
 
+const useRemote = (id: string, params?: Record<string, any>) => {
+  const { data } = useReactQuery([id, params]);
+  return data;
+};
+
+const useLocal = (id: string, params?: Record<string, any>) => {
+  const sub = subscriptions[id].fn;
+  return useStore((state: any) => sub(state, params));
+};
+
 export function useValue<T>(id: string, params?: Record<string, any>): T {
-  const { data } = useQuery([id, params]);
-  return data as T;
+  const useVal = subscriptions[id].async ? useRemote : useLocal;
+  return useVal(id, params) as T;
 }
+
+export function event(id: string, fn: any) {
+  events[id] = { fn };
+}
+
+export function useDispatch(id: string, options?: any): any {
+  const { emptyParams = false }: any = options || {};
+  const { fn } = events[id];
+  return useCallback(
+    (params?: Record<string, any>) => fn(useStore.setState, emptyParams ? {} : params || {}),
+    [fn, emptyParams]
+  );
+}
+
+event('inc', (setState: any, { n }: any) => {
+  console.log(n);
+  setState((state: any) => ({ bears: state.bears + n }));
+});
