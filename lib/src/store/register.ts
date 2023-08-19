5b457cb06a71e329c41019e2e63cf4fb42663a98
indy <kslvsunil@gmail.com>
2023-08-19 21:12:22 +0530

    add remoteSub

--- lib/src/store/register.ts
+++ lib/src/store/register.ts
@@ -1,7 +1,10 @@
 import { StoreApi } from 'zustand';
+import isEmpty from 'lodash/isEmpty';
 
 type Read = (args: { state: any; params?: any }) => any;
-type AsyncRead = (args: { params?: Record<string, any> }) => any;
+
+type AsyncRead<Params, Result> = (args: { params?: Params }) => Promise<Result>;
+
 export type Dispatch = (args: {
   setState: StoreApi<any>['setState'];
   getState: Function;
@@ -16,8 +19,22 @@ export function sub(id: string, fn: Read) {
   subscriptions[id] = { fn, async: false };
 }
 
-export function asyncSub(id: string, fn: AsyncRead) {
+export function asyncSub<Params, Result>(id: string, fn: AsyncRead<Params, Result>) {
   subscriptions[id] = { fn, async: true };
+}
+
+export function remoteSub<Params, Result>(id: string) {
+  asyncSub<Params, Result>(id, async ({ params }) => {
+    const key = encodeURIComponent(id);
+    const action = 'subscribe';
+    const urlJson = isEmpty(params) ? null : encodeURIComponent(JSON.stringify(params));
+    const url = `/api/rpc/${key}?action=${action}&json=${urlJson}`;
+
+    const res = await fetch(url);
+    const json = await res.json();
+
+    return json.data;
+  });
 }
 
 export function evt(id: string, fn: Dispatch) {
