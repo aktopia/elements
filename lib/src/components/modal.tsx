d280c5d0738d6e5be2591b14e6018fd4935b79f7
indy <kslvsunil@gmail.com>
2023-08-30 16:01:49 +0530

    Use radix ui dialog for Modal

--- lib/src/components/modal.tsx
+++ lib/src/components/modal.tsx
@@ -1,9 +1,10 @@
 import { XMark } from '@elements/icons';
-import { Dialog, Transition } from '@headlessui/react';
-import React, { Fragment } from 'react';
+import * as Dialog from '@radix-ui/react-dialog';
+import type { ReactNode } from 'react';
+import { useCallback } from 'react';
 
 interface ModalProps {
-  children: React.ReactNode;
+  children: ReactNode;
   onClose: (..._: any) => void;
   visible: boolean;
 }
@@ -12,13 +13,13 @@ export const ModalTitle = Dialog.Title;
 
 const Close = ({ onClose }: any) => {
   return onClose ? (
-    <div
+    <Dialog.Close
       className={
         'cursor-pointer p-1 text-gray-500 transition-all ease-out hover:rounded-full hover:bg-gray-100 hover:text-gray-700'
       }
       onClick={onClose}>
       <XMark className={'h-4 w-4'} />
-    </div>
+    </Dialog.Close>
   ) : null;
 };
 
@@ -37,47 +38,35 @@ export const ModalHeader = ({ title, onClose }: any) => {
   );
 };
 
-export const ModalPanel = ({ children }: { children: React.ReactNode }) => {
+export const ModalPanel = ({ children }: { children: ReactNode }) => {
   return (
-    <Transition.Child
-      as={Fragment}
-      enter={'ease-out duration-300'}
-      enterFrom={'opacity-0 scale-95'}
-      enterTo={'opacity-100 scale-100'}
-      leave={'ease-in duration-200'}
-      leaveFrom={'opacity-100 scale-100'}
-      leaveTo={'opacity-0 scale-95'}>
-      <div
-        className={
-          'w-max scale-100 transform overflow-hidden rounded-2xl border border-gray-200 bg-white opacity-100 shadow-xl transition-all'
-        }>
-        {children}
-      </div>
-    </Transition.Child>
+    <div
+      className={
+        'w-max scale-100 transform overflow-hidden rounded-2xl border border-gray-200 bg-white opacity-100 shadow-xl transition-all'
+      }>
+      {children}
+    </div>
   );
 };
+
 export const Modal = ({ children, onClose, visible }: ModalProps) => {
+  const onOpenChange = useCallback(
+    (open: boolean) => {
+      !open && onClose();
+    },
+    [onClose]
+  );
+
   return (
-    <Transition.Root appear afterLeave={console.log} as={Fragment} show={visible}>
-      <Dialog className={'relative z-30'} open={visible} onClose={onClose}>
-        <Transition.Child
-          as={Fragment}
-          enter={'ease-out duration-300'}
-          enterFrom={'opacity-0'}
-          enterTo={'opacity-100'}
-          leave={'ease-in duration-200'}
-          leaveFrom={'opacity-100'}
-          leaveTo={'opacity-0'}>
-          <div className={'fixed inset-0 bg-opacity-25 transition-opacity'} />
-        </Transition.Child>
-        <div
-          className={
-            'fixed inset-0 flex items-start justify-center overflow-y-auto p-4 sm:p-6 md:p-20'
-          }>
-          {children}
-        </div>
-      </Dialog>
-    </Transition.Root>
+    <Dialog.Root open={visible} onOpenChange={onOpenChange}>
+      <Dialog.Overlay />
+      <Dialog.Content
+        className={
+          'fixed inset-0 flex items-start justify-center overflow-y-auto p-4 sm:p-6 md:p-20'
+        }>
+        <div>{children}</div>
+      </Dialog.Content>
+    </Dialog.Root>
   );
 };
 
