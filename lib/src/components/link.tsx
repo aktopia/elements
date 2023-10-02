1653658c7b32b30657c93e4b7ac656053e89c321
indy <kslvsunil@gmail.com>
2023-10-02 23:31:56 +0530

    Typings for Link

--- lib/src/components/link.tsx
+++ lib/src/components/link.tsx
@@ -1,24 +1,27 @@
 import { navigateToPath } from '@elements/router';
 import useEvent from 'react-use-event-hook';
-import { forwardRef } from 'react';
+import { type AnchorHTMLAttributes, type ForwardedRef, forwardRef, type MouseEvent } from 'react';
 
-export const Link = forwardRef(({ href, children, onClick, ...props }: any, ref) => {
-  const onClick_ = useEvent((event: any) => {
-    // ignores the navigation when clicked using right mouse button or
-    // by holding a special modifier key: ctrl, command, win, alt, shift
-    if (event.ctrlKey || event.metaKey || event.altKey || event.shiftKey || event.button !== 0)
-      return;
+export const Link = forwardRef(
+  (
+    { href, children, onClick, ...props }: AnchorHTMLAttributes<HTMLAnchorElement>,
+    ref: ForwardedRef<HTMLAnchorElement>
+  ) => {
+    const onClick_ = useEvent((event: MouseEvent<HTMLAnchorElement>) => {
+      if (event.ctrlKey || event.metaKey || event.altKey || event.shiftKey || event.button !== 0)
+        return;
 
-    onClick && onClick(event);
-    if (!event.defaultPrevented) {
-      event.preventDefault();
-      navigateToPath(href);
-    }
-  });
+      onClick && onClick(event);
+      if (!event.defaultPrevented) {
+        event.preventDefault();
+        href && navigateToPath(href);
+      }
+    });
 
-  return (
-    <a href={href} onClick={onClick_} {...props} ref={ref}>
-      {children}
-    </a>
-  );
-});
+    return (
+      <a href={href} onClick={onClick_} {...props} ref={ref}>
+        {children}
+      </a>
+    );
+  }
+);
